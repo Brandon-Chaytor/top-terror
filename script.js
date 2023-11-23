c3100f8f02788a4f5a50167d508b9fd1ca7f7d28
Brandon-Chaytor <chaytorbrandon@gmail.com>
2023-11-22 20:47:01 -0330

    Display movies with button click

--- script.js
+++ script.js
@@ -17,48 +17,51 @@ const options = {
 //.then((response) => console.log(response))
 //.catch((err) => console.error(err));
 
-const message = "Hello, Console!";
+//const message = "Hello, Console!";
 
-console.log(message);
-
-let popularMovies = [];
-getPopularMovies();
-
-async function getPopularMovies() {
+async function getHorrorMovies() {
   try {
     const response = await fetch(
       "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
       options
     );
-    const data = await response.json(); // Extracts data as a json object from the response
+    const data = await response.json();
     const allMovies = data.results;
     const horrorMovies = allMovies.filter((movie) => {
-      return movie.genre_ids.includes(27);
+      return movie.genre_ids.includes(27); //includes or may include any movies with id 27,127, 1127 etc.. fix
     });
-    console.log(allMovies);
-    console.log(horrorMovies);
-    const sortedMovies = horrorMovies.sort((a, b) => {
-      a.data > b.data ? 1 : -1;
-    });
-    popularMovies = data.results;
+
+    return horrorMovies;
   } catch (err) {
     console.log(err);
   }
 }
 
-console.log(popularMovies);
+async function displayData() {
+  const data = await getHorrorMovies();
 
-function displayData(currentPage) {
-  const topterrorContainer = document.querySelector("topterror");
+  const movieContainer = document.querySelector(".movie-container");
+  movieContainer.innerHTML = "";
 
-  horrorMovies.forEach((movie) => {
-    const topTerrorData = document.createElement("div");
-    topTerrorData.innerHTML = (
-      <div class="top-terror">
-        <h5>Title: ${movie.title} </h5>
-        <p>Release Date: ${movie.release_date}</p>
-        <img src="${movie.poster_path}"></img>
-      </div>
-    );
+  data.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+    movieCard.innerHTML = `
+    <div class="movie-image">
+      <img
+        src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+        alt="${movie.title}"
+      />
+    </div>
+    <div class="movie-info">
+      <h3 class="movie-title">${movie.title}</h3>
+      <span class="movie-rating">${movie.vote_average}</span>
+    </div>
+    <div class="overview">
+      <h3>Overview</h3>
+      ${movie.overview}
+    </div>
+    `;
+    movieContainer.appendChild(movieCard);
   });
 }
